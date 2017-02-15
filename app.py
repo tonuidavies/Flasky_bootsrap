26643333ebbfd684b7015f506d8d61270e5815be
tonuidavies <tonuidavies@gmail.com>
2017-02-15 13:35:34 +0300

    now creating the models and running on terminal db.create_all() command for the table to be craeted

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask
-from flask sqlalchemy import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:password@localhost/flask_movies'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:password@localhost/flask_movie'
 
-db = SQLAlchemy
+db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
 
-# clear
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 if __name__ =="__main__":
     app.run()
