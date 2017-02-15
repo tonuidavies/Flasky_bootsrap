346040834b820320b86aa3f22f3f97d42516c33c
tonuidavies <tonuidavies@gmail.com>
2017-02-15 12:59:27 +0300

    connecting with postgres database has never been this simple

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 from flask import Flask
+from flask sqlalchemy import sqlalchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:password@localhost/flask_movies'
+
+db = SQLAlchemy
+
+
+# clear
+
+
+if __name__ =="__main__":
+    app.run()
+    debug = True
